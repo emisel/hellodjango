37854677a4956cb9bf9d6f30b3b60df487c13b00
emisel <eselmeryd@gmail.com>
2014-04-18 10:40:08 +0200

    prod settings 2

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -66,17 +66,17 @@ WSGI_APPLICATION = 'hellodjango.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 #settings used for dev
-# DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-#             'NAME': 'windowdb',                      # Or path to database file if using sqlite3.
-#             # The following settings are not used with sqlite3:
-#             'USER': 'superuser',
-#             'PASSWORD': 'superuser',
-#             'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
-#             'PORT': '',                      # Set to empty string for default.
-#         }
-# }
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': 'windowdb',                      # Or path to database file if using sqlite3.
+            # The following settings are not used with sqlite3:
+            'USER': 'superuser',
+            'PASSWORD': 'superuser',
+            'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+            'PORT': '',                      # Set to empty string for default.
+        }
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
